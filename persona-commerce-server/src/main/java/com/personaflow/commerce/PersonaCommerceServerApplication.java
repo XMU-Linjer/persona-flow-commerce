b29348fed274bd4f499ec3069023a951cd946ea9
Linker (XMU-Linjer) <884917500@qq.com>
2026-06-25 23:00:01 +0800

    feat: initialize Spring Boot server and MySQL connection 初始化Spring Boot服务器和MySQL连接

---
+++ persona-commerce-server/src/main/java/com/personaflow/commerce/PersonaCommerceServerApplication.java
@@ -0,0 +1,13 @@
+package com.personaflow.commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonaCommerceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonaCommerceServerApplication.class, args);
+	}
+
+}
---
+++ persona-commerce-server/src/test/java/com/personaflow/commerce/PersonaCommerceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personaflow.commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonaCommerceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
